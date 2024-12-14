e3efdfdaa29c46a5cd692ea645ff98a0883a25b1
imagic <315636696@qq.com>
2024-12-14 20:33:09 +0800

    build

--- src/views/root/index.vue
+++ src/views/root/index.vue
@@ -1,3 +1,32 @@
-<template>
-    Root
-</template>
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+const ipAddress = ref<string>('')
+
+const fetchInfo = ({ username, repo, issueNumber }: { username: string, repo: string, issueNumber: number }) => {
+  const url = `https://api.github.com/repos/${username}/${repo}/issues/${issueNumber}`
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error!')
+      }
+      return response.json()
+    })
+    .then(issueData => {
+      ipAddress.value = JSON.parse(issueData.body)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+onMounted(() => {
+  let matches = window.location.href.match(/https:\/\/([^\/]+)\.github\.io\/([^\/]+)\/#/)
+
+  if (matches) {
+    fetchInfo({ repo: matches[2], username: matches[1], issueNumber: 2 })
+  }
+})
+</script>
+
+<template>{{ ipAddress }}</template>
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     vueDevTools()
@@ -18,6 +19,7 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    outDir: './docs',
     rollupOptions: {
       output: {
         manualChunks: (id: string) => {
